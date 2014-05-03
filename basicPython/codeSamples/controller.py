bedbdf5ec47373d48c8012a670ba3be162d1d863
Ryan Walmsley <ryanteck@gmail.com>
2014-05-03 11:53:49 +0100

    On offs working

--- basicPython/codeSamples/controller.py
+++ basicPython/codeSamples/controller.py
@@ -11,12 +11,27 @@ while True:
 	keystate = pygame.key.get_pressed()
 	if keystate[pygame.K_RIGHT]:
         	print "right"
+	else:
+		print "right off"
+
         if keystate[pygame.K_LEFT]:
 		print "left"
+	else:
+		print "left off"
+	
 	if keystate[pygame.K_DOWN]:
 		print "down"
+	else:
+		print "down off"
+	
 	if keystate[pygame.K_UP]:
 		print "up"
+	else:
+		print "up off"
+
+	#pygame loop
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
             		exit(0)
+	time.sleep(0.01)
