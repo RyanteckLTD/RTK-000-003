4f26ea0a100ce00a4a5569b7b9f79d3730c63822
Ryan Walmsley <ryanteck@gmail.com>
2014-05-11 14:01:11 +0000

    Finished spacebrew client

--- spacebrewGuide/codeSamples/robotClient.py
+++ spacebrewGuide/codeSamples/robotClient.py
@@ -3,9 +3,11 @@
 #(C) Ryanteck LTD. 2014
 #Contributors: Ryan Walmsley, Michael Horne
 import time
-
 import RPi.GPIO as GPIO
+#Import spacebrew library
+from spacebrew import Spacebrew
 
+#Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -44,3 +46,42 @@ def stop():
         GPIO.output(22,0)
         GPIO.output(23,0)
 
+#setup Spacebrew
+name = "Cake"
+robotName = "RTK-000-003: "+ name
+brew = Spacebrew(robotName, server="sandbox.spacebrew.cc")
+
+brew.addSubscriber("forward", "boolean")
+brew.addSubscriber("backward", "boolean")
+brew.addSubscriber("left", "boolean")
+brew.addSubscriber("right", "boolean")
+
+def fwd_hndl(value):
+    # do something with the value received
+    forwards()
+    time.sleep(0.03)
+    stop()
+def bk_hndl(value):
+    backwards()
+    time.sleep(0.03)
+    stop()
+def lt_hndl(value):
+    left()
+    time.sleep(0.03)
+    stop()
+def rt_hndl(value):
+    right()
+    time.sleep(0.03)
+    stop()
+
+brew.subscribe("forward", fwd_hndl)
+brew.subscribe("backward", bk_hndl)
+brew.subscribe("left", lt_hndl)
+brew.subscribe("right", rt_hndl)
+
+try:
+    brew.start()
+    while True:
+        pass
+finally:
+    brew.stop()
